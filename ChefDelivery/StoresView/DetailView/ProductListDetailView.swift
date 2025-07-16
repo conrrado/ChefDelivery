0c73d131ebd8bfdd6c8d3bd541015f6b6b0b7d6e
Conrrado Camacho <con.webmaster@gmail.com>
2025-07-16 16:42:38 -0300

    Add Product Detail screen with modal presentation and quantity state
    - Implemented ProductDetailView to display product details
    - Used .sheet modifier to present the product detail screen as a modal
    - Added @State property to manage product quantity

--- ChefDelivery/StoresView/DetailView/ProductListDetailView.swift
+++ ChefDelivery/StoresView/DetailView/ProductListDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ProductListDetailView: View {
     
     let store: StoreType
+    @State private var selectedProduct: ProductType?
     
     var body: some View {
         Text("Produtos")
@@ -18,27 +19,37 @@ struct ProductListDetailView: View {
             .padding()
         
         ForEach(store.products) { product in
-            HStack(spacing: 8) {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(product.name)
-                        .bold()
+            
+            Button {
+                selectedProduct = product
+            } label: {
+                HStack(spacing: 8) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text(product.name)
+                            .bold()
+                        
+                        Text(product.description)
+                            .foregroundColor(.black.opacity(0.5))
+                            .multilineTextAlignment(.leading)
+                        
+                        Text(product.formattedPrice)
+                    }
                     
-                    Text(product.description)
-                        .foregroundColor(.black.opacity(0.5))
+                    Spacer()
                     
-                    Text(product.formattedPrice)
+                    Image(product.image)
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(12)
+                        .frame(width: 120, height: 120)
+                        .shadow(color: .black.opacity(0.3), radius: 20, x: 6, y: 8)
                 }
-                
-                Spacer()
-                
-                Image(product.image)
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(12)
-                    .frame(width: 120, height: 120)
-                    .shadow(color: .black.opacity(0.3), radius: 20, x: 6, y: 8)
+                .padding()
+                .foregroundColor(.black)
             }
-            .padding()
+            .sheet(item: $selectedProduct) { product in
+                ProductDetailView(product: product)
+            }
         }
     }
 }
